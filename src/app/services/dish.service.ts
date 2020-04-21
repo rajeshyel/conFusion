4d7dba6d18afe2852af37cce5b722bcb619e43b9
rajeshyel <rajesh.yelchuri@gmail.com>
2020-04-21 17:56:57 +0530

    Updated only dish.service.ts to use observables with promises,however, this commit is not part of the exercises. I am comitting this on my own so that i can refer the dish.service.ts as an example of how to write observables and convert them to promises

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
 import { resolve } from 'url';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +11,15 @@ import { resolve } from 'url';
 export class DishService {
 
   constructor() { }
-  getDishes(): Promise<Dish[]>{
-    return new Promise(resolve=>{
-      // Simulate server latency with 2 second delay
-      setTimeout(()=>resolve(DISHES),2000);
-    });
+  getDishes(): Promise<Dish[]> {
+    return of(DISHES).pipe(delay(2000)).toPromise();
   }
 
-  getDish(id:string):Promise<Dish>{
-    return new Promise(resolve=>{
-      // Simulate server latency with 2 second delay
-      setTimeout(()=>resolve(DISHES.filter((dish)=>(dish.id==id))[0]),2000);
-    });
+  getDish(id: string): Promise<Dish> {
+    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000)).toPromise();
   }
 
-  getFeaturedDish():Promise<Dish>{
-    //returns the dish which has "featured" as "true"
-    return new Promise(resolve=>{
-      // Simulate server latency with 2 second delay
-      setTimeout(()=>resolve(DISHES.filter((dish)=>dish.featured)[0]),2000);
-    });
+  getFeaturedDish(): Promise<Dish> {
+    return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000)).toPromise();
   }
 }
